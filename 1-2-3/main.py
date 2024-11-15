557462da0136e217178cceff855f8f7423f57c7f
Yolulboi13 <marcus.debarbieri@vtsd.com>
2024-11-15 13:11:12 -0500

    1-2-3, day 3? i think? did a lil bit o funky stuff, but i got apples w letters that fall and respawn gonna need to learn how to do that w multiple.

--- 1-2-3/main.py
+++ 1-2-3/main.py
@@ -1,7 +1,9 @@
 #   a123_apple_1.py
 import turtle as trtl
+import random as rand
 
 #-----setup-----
+
 apple_image = "apple.gif" # Store the file name of your shape
 
 wn = trtl.Screen()
@@ -13,25 +15,37 @@ apple.penup()
 fall_speed = int(input("Enter fall speed as a number: "))
 
 #-----functions-----
+
 # given a turtle, set that turtle to be shaped by the image file
-def draw_apple(active_apple):
-  active_apple.shape(apple_image)
+def draw_apple(apple):
+  apple.shape(apple_image)
+  global letter
+  letter = rand.choice(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"])
+  apple.goto(rand.randint(-150, 150), rand.randint(0, 100))
+  apple.showturtle()
+  apple.color("white")
+  apple.write(letter, font=("Arial", 20, "bold"))
   wn.update()
 
 def apple_gravity():
   apple_x = (apple.xcor())
   apple_y = (apple.ycor())
   apple.setheading(270)
+
   while apple_y > -200:
     apple.clear()
     apple_y -= fall_speed
     apple.goto(apple_x, apple_y)
-    apple.color("white")
-    apple.write("A", font=("Arial", 20, "bold"))
+    apple.write(letter, font=("Arial", 20, "bold"))
 
+  apple.hideturtle()
+  apple.clear()
+  draw_apple(apple)
 
 #-----function calls-----
+
 draw_apple(apple)
-wn.onkeypress(apple_gravity, "a")
+
+wn.onkeypress(apple_gravity)
 wn.listen()
 wn.mainloop()

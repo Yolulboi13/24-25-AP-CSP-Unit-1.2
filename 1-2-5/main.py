5b8ce6a01cf4871d345415aab6550f244b3d24d3
Yolulboi13 <marcus.debarbieri@vtsd.com>
2024-12-10 11:46:08 -0500

    1-2-5 go

--- 1-2-5/main.py
+++ 1-2-5/main.py
@@ -2,7 +2,21 @@
 
 # Imports
 import turtle as t
+import random as rd
 
 #-----Initialize Variables-----
 score = 0
 player_name = input("Input your name: ")
+wn = t.Screen()
+ball = t.Turtle()
+ball_direction = 90
+wn.bgcolor('black')
+ball.shape('circle')
+ball.color(rd.randint(50,255),rd.randint(50,255),rd.randint(50,255))
+
+
+
+
+
+
+wn.mainloop()

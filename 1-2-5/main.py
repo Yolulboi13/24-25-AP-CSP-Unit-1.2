d8e43f333c8711e32b40e3808c6b53a5219f088f
Yolulboi13 <marcus.debarbieri@vtsd.com>
2024-12-11 13:53:55 -0500

    1-2-5 start

--- 1-2-5/main.py
+++ 1-2-5/main.py
@@ -7,16 +7,52 @@ import random as rd
 #-----Initialize Variables-----
 score = 0
 player_name = input("Input your name: ")
+
 wn = t.Screen()
+wn.colormode(255)
+
+wn.bgcolor('white')
+
 ball = t.Turtle()
-ball_direction = 90
-wn.bgcolor('black')
-ball.shape('circle')
+#ball.shape('circle')
 ball.color(rd.randint(50,255),rd.randint(50,255),rd.randint(50,255))
+ball.setheading(45)
+ball.speed(3)
+ball.penup()
 
 
+paddle = t.Turtle()
+paddle.speed('fastest')
+paddle.goto(0,100)
 
+#-----functions-----
+def bounce_wall():
+    if ball.heading() == 45:
+        ball.left(90)
+        print('bounce')
+    elif ball.heading() == 135:
+        ball.right(90)
+    elif ball.heading() == 225:
+        ball.left(90)
+    elif ball.heading() == 315:
+        ball.right(90)
+def bounce_ceiling():
+    if ball.heading == 45 or ball.heading == 135:
+        ball.left(180)
 
+def loser():
+    wn.mainloop()
+def winner():
+    print("yay")
 
+#-----events-----
+while True:
+    ball.forward(2)
+    if ball.ycor() <= paddle.ycor():
+        #loser()
+        paddle.goto(0,150)
+    elif ball.xcor() >= -150 or ball.xcor() >= 150:
+        bounce_wall()
+    elif ball.ycor() >= 150:
+        bounce_ceiling()
 
-wn.mainloop()

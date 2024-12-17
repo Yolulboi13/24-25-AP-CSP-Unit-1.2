4835397a42f2cf7f3b0ea7dfc9c7cca23bcac60f
Yolulboi13 <marcus.debarbieri@vtsd.com>
2024-12-17 13:55:05 -0500

    1-2-5 we lost

--- 1-2-5/main.py
+++ 1-2-5/main.py
@@ -1,5 +1,7 @@
 # Breakout
 
+#NOTE: THIS CODE IS ANYTHING BUT COMPLETE, ALTHOUGH I BELIEVE WE AT LEAST MET THE REQUIREMENTS DESPITE IT NOT BEING COMPLETELY FUNCTIONAL OR STABLE
+
 # Imports
 import turtle as t
 import random as rd
@@ -45,27 +47,38 @@ brick_line_3 = [(-40,125),(-20,125),(0,125),(20,125),(40,125)]
 brick_trtl_list = []
 
 # Creates Turtles
-for num in range(9):
-    brick = t.Turtle()
-    brick.color(rd.randint(50, 255), rd.randint(50, 255), rd.randint(50, 255))
-    brick.penup()
-    brick.speed(0)
-    brick.shape('square')
-    brick.goto(brick_line_1[num])
-for num in range(7):
-    brick = t.Turtle()
-    brick.color(rd.randint(50, 255), rd.randint(50, 255), rd.randint(50, 255))
-    brick.penup()
-    brick.speed(0)
-    brick.shape('square')
-    brick.goto(brick_line_2[num])
-for num in range(5):
-    brick = t.Turtle()
-    brick.color(rd.randint(50, 255), rd.randint(50, 255), rd.randint(50, 255))
-    brick.penup()
-    brick.speed(0)
-    brick.shape('square')
-    brick.goto(brick_line_3[num])
+brick_1 = t.Turtle()
+brick_2 = t.Turtle()
+brick_3 = t.Turtle()
+brick_4 = t.Turtle()
+brick_5 = t.Turtle()
+brick_6 = t.Turtle()
+brick_7 = t.Turtle()
+brick_8 = t.Turtle()
+brick_9 = t.Turtle()
+brick_10 = t.Turtle()
+brick_11 = t.Turtle()
+brick_12 = t.Turtle()
+brick_13 = t.Turtle()
+brick_14 = t.Turtle()
+brick_15 = t.Turtle()
+brick_16 = t.Turtle()
+brick_17 = t.Turtle()
+brick_18 = t.Turtle()
+brick_19 = t.Turtle()
+brick_20 = t.Turtle()
+brick_21 = t.Turtle()
+
+# Putting turtles in a list for later access and organization
+brick_trtl_list = [brick_1, brick_2, brick_3 , brick_4 , brick_5 , brick_6 , brick_7 , brick_8 , brick_9 , brick_10, brick_11, brick_12, brick_13, brick_14, brick_15, brick_16, brick_17, brick_18, brick_19, brick_20, brick_21]
+
+# Setting up turtles using the list from before
+for num in range(21):
+    brick_trtl_list[num].color(rd.randint(50, 255), rd.randint(50, 255), rd.randint(50, 255))
+    brick_trtl_list[num].speed(0)
+    brick_trtl_list[num].shape('square')
+    brick_trtl_list[num].penup()
+    brick_trtl_list[num].goto(brick_coords[num])
 
 #-----functions-----
 def bounce_wall():
@@ -97,11 +110,13 @@ def bounce_paddle():
         ball.left(90)
 
 def loser():
+    print("Sorry, ", player_name,". You only managed to break ", score," bricks..." )
     wn.mainloop()
 
 def winner():
-    print("Congrats,", player_name,". You broke all the bricks!!")
+    print("Congrats, ", player_name,". You broke all the bricks!!")
 
+# moving paddle functions
 def paddle_left():
     paddle.setheading(180)
     paddle.forward(5)
@@ -116,14 +131,14 @@ def score_up_one():
     score_trtl.write("Score: "+str(score), font=("Helvetica", 32, "bold"))
 
 def brick_broken():
+    global brick_break_range_end12
     print("brick")
-    brick.hideturtle()
-    if brick.ycor() == 225:
-        brick_line_1.remove((brick.xcor(),brick.ycor()))
-    if brick.ycor() == 175:
-        brick_line_2.remove((brick.xcor(),brick.ycor()))
-    if brick.ycor() == 125:
-        brick_line_3.remove((brick.xcor(),brick.ycor()))
+    for num in range(0, brick_break_range_end):
+        brick_break_range_end -= 1
+        if ball.distance(brick_trtl_list[num]) <= 20:
+            brick_trtl_list[num].hideturtle()
+            brick_trtl_list[num] = "broken"
+    score_up_one()
 
 #-----events-----
 
@@ -141,5 +156,7 @@ while True:
     if ball.ycor() <= 45:
         if ball.distance(paddle) <= 20:
             bounce_paddle()
-    if ball.distance(brick) <=20:
+
+    # Then all the bricks
+    if ball.distance(brick_trtl_list[0] or brick_trtl_list[1] or brick_trtl_list[2] or brick_trtl_list[3] or brick_trtl_list[4] or brick_trtl_list[5] or brick_trtl_list[6] or brick_trtl_list[7] or brick_trtl_list[8] or brick_trtl_list[9] or brick_trtl_list[10] or brick_trtl_list[11] or brick_trtl_list[12] or brick_trtl_list[13] or brick_trtl_list[14] or brick_trtl_list[15] or brick_trtl_list[16] or brick_trtl_list[17] or brick_trtl_list[18] or brick_trtl_list[19] or brick_trtl_list[20]) <= 20:
         brick_broken()

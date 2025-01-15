55281e6ed15370ccb4e8758caf71c8926b085f45
Yolulboi13 <marcus.debarbieri@vtsd.com>
2025-01-15 13:53:03 -0500

    project day 3

--- AP Test Create Project/formula_calc_II.py
+++ AP Test Create Project/formula_calc_II.py
@@ -2,13 +2,17 @@ import math
 
 history = []
 
+def cube_root(num):
+    return num ** (1/3)
 
+def half_root(num):
+    return math.sqrt(num/2)
 
 def cubic_formula():
-    a = int(input("Please enter the value for A: "))
-    b = int(input("Please enter the value for B: "))
-    c = int(input("Please enter the value for C: "))
-    d = int(input("Please enter the value for D: "))
+    a = float(input("Please enter the value for A: "))
+    b = float(input("Please enter the value for B: "))
+    c = float(input("Please enter the value for C: "))
+    d = float(input("Please enter the value for D: "))
     frac_1 = ((-b**3)/(27*a**3))
     frac_2 = ((b*c)/(6*a**2))
     frac_3 = (d/(2*a))
@@ -17,7 +21,7 @@ def cubic_formula():
     frac_6 = (b/(3*a))
     result = cube_root((frac_1+frac_2-frac_3) + math.sqrt((frac_1+frac_2-frac_3)**2) + ((frac_4-frac_5)**3) + (frac_1+frac_2-frac_3) + math.sqrt((frac_1+frac_2-frac_3)**2) + ((frac_4-frac_5)**3) - frac_6)
     print('''
-    The result is ''' + str(result) +'''
+    The result for x is ''' + str(result) +'''
     ''')
     history.append("Cubic Formula: " + str(result))
 
@@ -26,18 +30,49 @@ def view_history():
     '''+str(history)+'''
     ''')
 
-def cube_root(num):
-    return num ** (1/3)
+def herons_formula():
+    a = float(input("Please enter the value for A: "))
+    b = float(input("Please enter the value for B: "))
+    c = float(input("Please enter the value for C: "))
+    s = (a+b+c)/2
+    result = (s*(s-a)*(s-b)*(s-c)) ** (1/2)
+    print('''
+    The result is ''' + str(result) +'''
+    ''')
+    history.append("Heron's Formula: " + str(result))
+
+def sphere_surface_area():
+    r = float(input("Please enter the radius of the sphere: "))
+    result = 4*math.pi*(r**2)
+    print('''
+    The result is ''' + str(result) +'''
+    ''')
+    history.append("Sphere Surface Area: " + str(result))
+
+def quartic_formula():
+    a = float(input("Please enter the value for A: "))
+    b = float(input("Please enter the value for B: "))
+    c = float(input("Please enter the value for C: "))
+    d = float(input("Please enter the value for D: "))
+    e = float(input("Please enter the value for E: "))
+    the_big_one = (2*(b**3)) - (9*a*b*c) + (27*(c**2)) + (27*(a**2)*d) - (72*b*d) + math.sqrt(-4*(((b**2)-(3*a*c)+(12*d))**3) + (((2*(b**3)) - (9*a*b*c) + (27*(c**2)) + (27*(a**2)*d) - (72*b*d)))**2)**(1/3)
+    result = (-a/4) - 0.5 * math.sqrt( (((-a**2)-4)-((2*b)/3)+(((2**(1/3))*((b*2)-(3*a*c)+(12*d)))/3*(the_big_one))+(the_big_one/(32**(1/3)))) -   )))
 
 print("Welcome to the Cubic Formula Calculator!")
 while True:
     choice = int(input('''What would you like to do?
 
     1. Cubic Formula
-    2. View History
+    2. Heron's Formula
+    3. Surface Area of a Sphere
+    4. View History
 
     '''))
     if choice == 1:
         cubic_formula()
     if choice == 2:
+        herons_formula()
+    if choice == 3:
+        sphere_surface_area()
+    if choice == 4:
         view_history()

aae862f0ed36b46fed7b85b60efd01e26495ef41
Yolulboi13 <marcus.debarbieri@vtsd.com>
2025-01-17 12:23:09 -0500

    project day 5 screw the quartic formula

--- AP Test Create Project/formula_calc_II.py
+++ AP Test Create Project/formula_calc_II.py
@@ -5,9 +5,6 @@ history = []
 def cube_root(num):
     return num ** (1/3)
 
-def half_root(num):
-    return math.sqrt(num/2)
-
 def cubic_formula():
     a = float(input("Please enter the value for A: "))
     b = float(input("Please enter the value for B: "))
@@ -49,25 +46,23 @@ def sphere_surface_area():
     ''')
     history.append("Sphere Surface Area: " + str(result))
 
-def quartic_formula():
+def quadratic_formula():
     a = float(input("Please enter the value for A: "))
     b = float(input("Please enter the value for B: "))
     c = float(input("Please enter the value for C: "))
-    d = float(input("Please enter the value for D: "))
-    e = float(input("Please enter the value for E: "))
-    the_big_one = (2*(b**3)) - (9*a*b*c) + (27*(c**2)) + (27*(a**2)*d) - (72*b*d) + math.sqrt(-4*( ( (b**2) - (3*a*c) + (12*d) )**3) + ( ( (2*(b**3)) - (9*a*b*c) + (27*(c**2)) + (27*(a**2)*d) - (72*b*d)))**2)**(1/3)
-    result = ((-a/4) - 0.5 * math.sqrt( ( ((-a**2)/2) - ((2*b)/3) + ( ( (2**(1/3)) * ((b*2) - (3*a*c) + (12*d)) ) / 3 * (the_big_one) ) + (the_big_one / (32**(1/3)) )) - (1/2) * math.sqrt(( ((-a**2)/2) - ((4*b)/3) + (( (2**(1/3)) * ((b*2) - (3*a*c) + (12*d))) / 3 * the_big_one ) - (the_big_one/(32**(1/3))) - ((-a(**3)) + (4*a*b) - (8*c)) / (4 * math.sqrt(( ((-a**2)/2) - ((2*b)/3) + ( ( (2**(1/3)) * ((b*2) - (3*a*c) + (12*d)) ) / 3 * (the_big_one) ) + (the_big_one / (32**(1/3)) ))))))))
+    result = -b + math.sqrt((b**2)-(4*a*c)) / (2*a)
+    result_2 = -b - math.sqrt((b**2)-(4*a*c)) / (2*a)
     print('''
-    The result is ''' + str(result) +'''
+    The result is ''' + str(result) +''' and ''' + str(result_2)+'''
     ''')
-    history.append("Quartic Formula: " + str(result))
+    history.append("Quadratic Formula: " + str(result) + ", " + str(result_2))
 
-print("Welcome to the Cubic Formula Calculator!")
+print("Welcome to the Second Formula Calculator!")
 while True:
     choice = int(input('''What would you like to do?
 
     1. Cubic Formula
-    2. Quartic Formula
+    2. Quadratic Formula
     3. Heron's Formula
     4. Surface Area of a Sphere
     5. View History
@@ -76,7 +71,7 @@ while True:
     if choice == 1:
         cubic_formula()
     if choice == 2:
-        quartic_formula()
+        quadratic_formula()
     if choice == 3:
         herons_formula()
     if choice == 4:

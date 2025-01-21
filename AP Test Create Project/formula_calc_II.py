8bdb95a00c78b0ed926b73223df2a19d25a0a3ae
Yolulboi13 <marcus.debarbieri@vtsd.com>
2025-01-21 11:45:58 -0500

    project day 6 we love the quartic formula

--- AP Test Create Project/formula_calc_II.py
+++ AP Test Create Project/formula_calc_II.py
@@ -51,7 +51,9 @@ def quadratic_formula():
     b = float(input("Please enter the value for B: "))
     c = float(input("Please enter the value for C: "))
     if math.sqrt((b**2)-(4*a*c)) < 0:
-        print("This has no zeros.")
+        print('''
+        This has no zeros.
+        ''')
     else:
         result = -b + math.sqrt((b**2)-(4*a*c)) / (2*a)
         result_2 = -b - math.sqrt((b**2)-(4*a*c)) / (2*a)
@@ -60,24 +62,44 @@ def quadratic_formula():
         ''')
         history.append("Quadratic Formula: " + str(result) + ", " + str(result_2))
 
+def quartic_formula():
+    a = float(input("Please enter the value for A: "))
+    b = float(input("Please enter the value for B: "))
+    c = float(input("Please enter the value for C: "))
+    d = float(input("Please enter the value for D: "))
+    the_big_one = (2*(b**3)) - (9*a*b*c) + (27*(c**2)) + (27*(a**2)*d) - (72*b*d) + math.sqrt(-4*( ( (b**2) - (3*a*c) + (12*d) )**3) + ( ( (2*(b**3)) - (9*a*b*c) + (27*(c**2)) + (27*(a**2)*d) - (72*b*d)))**2)**(1/3)
+    if (-4*(((b**2)-(3*a*c)+(12*d))**3)+(((2*(b**3))-(9*a*b*c)+(27*(c**2))+(27*(a**2)*d)-(72*b*d)))**2) < 0 or ( ( ((-a**2)/2) - ((2*b)/3) + ( ( (2**(1/3)) * ((b*2) - (3*a*c) + (12*d)) ) / 3 * (the_big_one) ) + (the_big_one / (32**(1/3)) )) - (1/2) * ( ((-a**2)/2) - ((4*b)/3) + (( (2**(1/3)) * ((b*2) - (3*a*c) + (12*d))) / 3 * the_big_one ) - (the_big_one/(32**(1/3))) - ((-a**3)) + (4*a*b) - (8*c) / 4)) < 0 or (((-a**2)/2) - ((2*b)/3) + ( ( (2**(1/3)) * ((b*2) - (3*a*c) + (12*d)) ) / 3 * (the_big_one) ) + (the_big_one / (32**(1/3)) )) < 0:
+        print('''
+        This has no zeros.
+        ''')
+    else:
+        result = ((-a/4) - 0.5 * math.sqrt( ( ((-a**2)/2) - ((2*b)/3) + ( ( (2**(1/3)) * ((b*2) - (3*a*c) + (12*d)) ) / 3 * (the_big_one) ) + (the_big_one / (32**(1/3)) )) - (1/2) * math.sqrt(( ((-a**2)/2) - ((4*b)/3) + (( (2**(1/3)) * ((b*2) - (3*a*c) + (12*d))) / 3 * the_big_one ) - (the_big_one/(32**(1/3))) - ((-a**3)) + (4*a*b) - (8*c)) / (4 * math.sqrt(( ((-a**2)/2) - ((2*b)/3) + ( ( (2**(1/3)) * ((b*2) - (3*a*c) + (12*d)) ) / 3 * (the_big_one) ) + (the_big_one / (32**(1/3)) )))))))
+        print('''
+        The result is ''' + str(result) +'''
+        ''')
+        history.append("Quartic Formula: " + str(result))
+
 print("Welcome to the Second Formula Calculator!")
 while True:
     choice = int(input('''What would you like to do?
 
-    1. Cubic Formula
-    2. Quadratic Formula
-    3. Heron's Formula
-    4. Surface Area of a Sphere
-    5. View History
+    1. Quartic Formula
+    2. Cubic Formula
+    3. Quadratic Formula
+    4. Heron's Formula
+    5. Surface Area of a Sphere
+    6. View History
 
     '''))
     if choice == 1:
-        cubic_formula()
+        quartic_formula()
     if choice == 2:
-        quadratic_formula()
+        cubic_formula()
     if choice == 3:
-        herons_formula()
+        quadratic_formula()
     if choice == 4:
-        sphere_surface_area()
+        herons_formula()
     if choice == 5:
+        sphere_surface_area()
+    if choice == 6:
         view_history()
